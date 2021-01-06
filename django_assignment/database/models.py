1275d8b05d00743f6bef779860d5f92f305f5e3b
Isaac Chung <ichung08@students.cs.ubc.ca>
2021-01-05 19:07:42 -0800

    task 6 - upload form

--- django_assignment/database/models.py
+++ django_assignment/database/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Sample(models.Model):
     sample_name = models.CharField(max_length=200)
-    description = models.TextField(default='')
-    time = models.DateTimeField(default='')
+    description = models.TextField()
+    time = models.DateTimeField(default=timezone.now)  
+
+    def __str__(self):
+        return self.sample_name
--- django_assignment/database/views.py
+++ django_assignment/database/views.py
@@ -1,11 +1,47 @@
 from django.shortcuts import render 
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import permission_required
+from .models import Sample
+import csv, io
 
 def download(request):
-    return render(request, 'download.html')
+    template = 'download.html'
+
+    return render(request, template)
 
 def search(request):
-    return render(request, 'search.html')
+    template = 'search.html'
 
+    return render(request, template)
+
+
+@permission_required('admin.can_add_log_entry')
 def upload(request):
-    return render(request, 'upload.html')
+    template = 'upload.html'
+
+    prompt = {
+        'order': 'Order of the CSV should be sample_name, description, time'
+    }
+
+    if request.method == 'GET':
+        return render(request, template, prompt)
+
+    csv_file = request.FILES['file']
+
+    if not csv_file.name.endswith('.csv'):
+        messages.error(request, "This is not a csv file")
+    
+    data_set = csv_file.read().decode('UTF-8')
+    io_string = io.StringIO(data_set)
+    next(io_string)
+    for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+        _, created = Sample.objects.update_or_create(
+            sample_name=column[0],
+            description=column[1]
+        )
+
+    context = {
+        'samples': Sample.objects.all()
+    }
+
+    return render(request, template, context)
