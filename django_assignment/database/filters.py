c9d7b4f98c2f188cc49f1fdd92d0864263a7677b
Isaac Chung <ichung08@students.cs.ubc.ca>
2021-01-18 01:20:08 -0800

    updated search

--- django_assignment/database/filters.py
+++ django_assignment/database/filters.py
@@ -1,11 +1,14 @@
 import django_filters
-from django_filters import DateFilter
+from django_filters import DateFilter, CharFilter
 
 from .models import *
 
 class SampleFilter(django_filters.FilterSet):
     start_date = DateFilter(field_name='time', lookup_expr='gte')
     end_date = DateFilter(field_name='time', lookup_expr='lte')
+    sample_name = CharFilter(field_name='sample_name', lookup_expr='icontains')
+    description = CharFilter(field_name='description', lookup_expr='icontains')
+    
     class Meta:
         model = Sample
         fields = '__all__'
